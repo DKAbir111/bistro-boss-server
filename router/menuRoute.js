444e502bd2d802c448eee2231694ca22644e7590
Darun Karas <darunkaras@Daruns-MacBook-Air.local>
2025-01-11 22:54:19 +0600

    delete update items route done with a error handle

--- router/menuRoute.js
+++ router/menuRoute.js
@@ -2,6 +2,7 @@ const express = require('express')
 
 const router = express.Router()
 const userVerification = require('../middleware/verifyUser')
+const { ObjectId } = require('mongodb')
 const createMenuRoute = (menuCollections, userCollections) => {
     //middleware
     const adminVerification = async (req, res, next) => {
@@ -34,11 +35,57 @@ const createMenuRoute = (menuCollections, userCollections) => {
         res.json(menuItems);
     })
 
+    //create new item
     router.post('/menu', userVerification, adminVerification, async (req, res) => {
         const newItem = req.body
         const result = await menuCollections.insertOne(newItem);
         res.json(result);
     })
+
+    //delete
+    router.delete('/menu/:id', async (req, res) => {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) }
+        const result = await menuCollections.deleteOne(filter)
+        res.send(result);
+    })
+
+    // get by id
+    router.get('/menu/:id', async (req, res) => {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) }
+        const result = await menuCollections.findOne(filter)
+        if (result) {
+            return res.send(result);
+        }
+        const result2 = await menuCollections.findOne({ _id: id })
+        return res.send(result2);
+
+    })
+
+    //patch item
+
+    router.patch('/menu/:id', async (req, res) => {
+        const id = req.params.id;
+        const newItem = req.body
+        const filter = { _id: new ObjectId(id) }
+        const updateDoc = {
+            $set: {
+                name: newItem.name,
+                price: newItem.price,
+                recipe: newItem.recipe,
+                category: newItem.category,
+                image: newItem.image,
+            }
+        }
+        const result = await menuCollections.updateOne(filter, updateDoc)
+        if (result.matchedCount === 0) {
+            const result2 = await menuCollections.updateOne({ _id: id }, updateDoc)
+            return res.send(result2);
+        }
+        res.send(result);
+    })
+
 
     return router;
 }
